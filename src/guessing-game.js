ee6cde2669e014a847ab0795c5f1883e7e688784
Disembow <e_naletov@mail.ru>
2023-08-28 10:19:37 +0200

    refactor: commit for commit

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,23 +1,23 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
-        this.min = min;
-        this.max = max;
-    }
-    
-    guess() {
-        this.mid = Math.round(( this.min + this.max ) / 2 );
-        return this.mid;
-    }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    lower() {
-        this.max = this.mid;
-    }
+  guess() {
+    this.mid = Math.round((this.min + this.max) / 2);
+    return this.mid;
+  }
 
-    greater() {
-        this.min = this.mid;
-    }
+  lower() {
+    this.max = this.mid;
+  }
+
+  greater() {
+    this.min = this.mid;
+  }
 }
 
 module.exports = GuessingGame;
